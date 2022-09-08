5cc2a468118417dda4e1791df81779c3078c8c8a
BaharRanjbar <baharranjbardev@gmail.com>
2022-09-08 09:34:02 +0430

    the Yellow Question file was added

--- 1.Yellow Questions/YellowQuestion.py
+++ 1.Yellow Questions/YellowQuestion.py
@@ -1,4 +1,4 @@
-while True:
+""" while True:
     try:
         n = int(input('please enter a number in range 1 to 10 :'))
         if n < 1 or n > 10:
@@ -6,8 +6,8 @@ while True:
         break
     except ValueError:
         print("Invalid integer. The number must be in the range of 1-10.")
-
-  
+ """
+n =int(input()) 
 print('W' , end="")
 for i in range(1, n+1):
     print ("o" , end="")
